8d7567276769f08a817022e9dd0cca40850d4f28
bhavani360 <65596064+bhavani360@users.noreply.github.com>
2022-11-03 15:39:17 +0530

    Add files via upload

---
+++ fpga/codes/flash_m4.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo python3 /home/bhavani/Documents/fpga/TinyFPGA-Programmer-Application/tinyfpga-programmer-gui.py --port /dev/ttyACM1  --appfpga /home/bhavani/Documents/fpga/helloworldfpga.bin --mode fpga
